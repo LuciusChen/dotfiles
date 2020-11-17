100eb363aa2387a501cf2727c82c78cc43ae5875
Lucius Chen <luciuschen@Luciuss-MacBook-Pro.local>
2020-11-17 14:18:21 +0800

    update

---
+++ .luarocks/config-5.4.lua
@@ -0,0 +1,11 @@
+lua_version = "5.4"
+lua_interpreter = "lua"
+rocks_trees = {
+    {
+        name = "user",
+        root = "/Users/luciuschen/.luarocks"
+    },
+}
+variables = {
+    LUA_DIR = "/Users/luciuschen/lua"
+}

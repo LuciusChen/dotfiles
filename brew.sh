e271eb8af33f002d1d2434f594db59cadd0d8fd2
Lucius Chen <luciuschen@Luciuss-MacBook-Pro.local>
2024-06-13 10:50:41 +0800

    update

--- brew.sh
+++ brew.sh
@@ -1,6 +1,7 @@
 brew tap daipeihust/tap
 brew tap d12frosted/emacs-plus
 brew install emacs-plus@30 --with-savchenkovaleriy-big-sur-icon --with-xwidgets
+brew install --cask lunar
 brew install starship eza mactex pngpaste
 brew install ffmpeg cmake fzf gnupg brew install ffmpeg cmake fzf gnupg inkscape
 brew install hugo zsh-autosuggestions zsh-syntax-highlighting jdtls lua maven mpv neofetch btop node pnpm pandoc pass pass-otp zbar pinentry-mac ripgrep fd tomcat@8

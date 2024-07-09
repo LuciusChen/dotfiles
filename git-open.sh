357670bb101a3e4f22de1455bccab98235729801
Lucius Chen <luciuschen@Luciuss-MacBook-Pro.local>
2024-07-09 20:47:38 +0800

    update

--- git-open.sh
+++ git-open.sh
@@ -3,7 +3,7 @@
 dirty=`git status --porcelain -uno | sed s/^...//`
 last_modified=`git show --pretty="format:" --name-only HEAD`
 
-if [ -n "$dirty" ]; then 
+if [ -n "$dirty" ]; then
     echo $dirty
 else
     echo $last_modified

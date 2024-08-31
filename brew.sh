e50afa5477426d7ea83dc3f0396b9772829eca1b
Lucius Chen <luciuschen@Luciuss-MacBook-Pro.local>
2024-08-31 16:18:05 +0800

    chore(brew.sh): add latexindent to brew installation script

--- brew.sh
+++ brew.sh
@@ -2,7 +2,7 @@ brew tap daipeihust/tap
 brew tap d12frosted/emacs-plus
 brew install emacs-plus@30 --with-savchenkovaleriy-big-sur-icon --with-xwidgets
 brew install --cask lunar
-brew install starship eza mactex pngpaste
+brew install starship eza mactex pngpaste latexindent
 brew install ffmpeg cmake fzf gnupg brew install ffmpeg cmake fzf gnupg inkscape
 brew install hugo zsh-autosuggestions zsh-syntax-highlighting jdtls lua maven mpv neofetch btop node pnpm pandoc pass pass-otp zbar pinentry-mac ripgrep fd tomcat@8
 brew install hammerspoon kitty iina

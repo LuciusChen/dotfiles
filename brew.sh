1eae1f0f81a58726b791e40ce8ba43069dd0d6bc
Lucius Chen <luciuschen@Luciuss-MacBook-Pro.local>
2024-09-18 15:51:21 +0800

    chore: update brew.sh script to include new installations
    - Added installation of inkscape, luarocks.
    - Removed duplicate installation of ffmpeg, cmake, fzf, gnupg.

--- brew.sh
+++ brew.sh
@@ -1,12 +1,13 @@
 brew tap daipeihust/tap
 brew tap d12frosted/emacs-plus
 brew install emacs-plus@30 --with-savchenkovaleriy-big-sur-icon --with-xwidgets
-brew install --cask lunar
+brew install --cask lunar inkscape
 brew install starship eza mactex pngpaste latexindent
-brew install ffmpeg cmake fzf gnupg brew install ffmpeg cmake fzf gnupg inkscape
+brew install ffmpeg cmake fzf gnupg
 brew install hugo zsh-autosuggestions zsh-syntax-highlighting jdtls lua maven mpv neofetch btop node pnpm pandoc pass pass-otp zbar pinentry-mac ripgrep fd tomcat@8
 brew install hammerspoon kitty iina
-brew install jdtls debugpy wget
+brew install jdtls wget
+brew install luarocks
 brew install pipx
 brew install vscode-langservers-extracted
 brew install isort black google-java-format prettier stylua

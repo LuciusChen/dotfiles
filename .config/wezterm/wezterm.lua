656f0b14d6641e65856d69e56bb777a40e042a48
Lucius <chenyh572@gmai.com>
2025-06-17 16:48:04 +0800

    feat(wezterm): change primary font to "Aporetic Serif Mono"

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -19,7 +19,7 @@ end)
 local config = wezterm.config_builder()
 
 config.color_scheme = "Tokyo Night"
-config.font = wezterm.font_with_fallback({ { family = "PragmataPro Mono Liga" }, "LXGW WenKai" })
+config.font = wezterm.font_with_fallback({ { family = "Aporetic Serif Mono" }, "LXGW WenKai" })
 config.font_size = 14
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"

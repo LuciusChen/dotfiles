50f527b789e2a15d2ceb224ce7bb630499885465
lucius <chenyh572@gmail.com>
2025-06-30 14:39:38 +0800

    feat(rofi): update font size and add icon display in Rofi configuration and script

--- arch/.config/rofi/init.sh
+++ arch/.config/rofi/init.sh
@@ -4,11 +4,11 @@
 if pgrep -f "rofi -dmenu" > /dev/null; then
     # If Rofi is running, kill it
     pkill -f "rofi -dmenu"
-    rofi -show drun
+    rofi -show drun -show-icons
 elif pgrep -x "rofi" > /dev/null; then
     # If Rofi is running in drun mode, kill it
     pkill -x "rofi"
 else
     # If Rofi is not running, start it in drun mode
-    rofi -show drun
+    rofi -show drun -show-icons
 fi
